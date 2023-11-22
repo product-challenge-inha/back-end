1f136f7ce9cbb18ee051838680b5f8c98e34cd14
Kohyeonjeong <toyu7870@naver.com>
2023-11-23 01:55:20 +0900

    feat: jpa 테이블 맵핑 셋팅

--- src/main/java/com/productchallenge/productchallenge/ProductChallengeApplication.java
+++ src/main/java/com/productchallenge/productchallenge/ProductChallengeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductChallengeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("실행 TEST");
 		SpringApplication.run(ProductChallengeApplication.class, args);
 	}
 
